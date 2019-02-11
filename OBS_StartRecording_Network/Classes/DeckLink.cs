ed3f7c88d6feaabf1d76352feef09db1986fd622
Joe Kelly <jojoguy10@gmail.com>
2019-02-10 20:49:42 -0800

    Working on new code

---
+++ OBS_StartRecording_Network/Classes/DeckLink.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace OBS_StartRecording_Network
+{
+    class DeckLink
+    {
+        TcpClient client;
+        NetworkStream stream;
+
+        public DeckLink(string ipAddress, int port)
+        {
+            client = new TcpClient(ipAddress, port);
+            stream = client.GetStream();
+        }
+
+        public void Write(string command)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(command);
+            stream.Write(data, 0, data.Length);
+        }
+
+        public string Read()
+        {
+            byte[] data = new byte[256];
+            int bytes = stream.Read(data, 0, data.Length);
+            return Encoding.ASCII.GetString(data, 0, bytes);
+        }
+    }
+}
